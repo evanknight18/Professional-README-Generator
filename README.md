00703ff5b8eed9b562ec8a489f0014ae87ed9cc3
Evan <evanknight18@gmail.com>
2023-04-24 18:16:54 -0600

    generateReadme const completed

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Professional-README-Generator
+
+# Profession READme Generator
+
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+
+## Description
+
+This project will generate a complete READme based on user input
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+1. Clone repo
+
+## Usage
+
+Bingy Bing
+
+## License
+
+This project is licensed under the terms of the MIT License.
+
+## Contributing
+
+Don't
+
+## Tests
+
+asdfasdf
+
+## Questions
+
+If you have any questions, please reach out:
+
+- [GitHub Profile](https://github.com/evanknight18)
+- Email: evanknight18@gmail.com
--- index.js
+++ index.js
@@ -11,3 +11,51 @@ const generateLicenseBadge = (license) => {
       return "";
   }
 };
+
+const generateReadme = (answers) => `
+# ${answers.title}
+
+![License](${generateLicenseBadge(answers.license)})
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## License
+
+This project is licensed under the terms of the ${answers.license} License.
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## Questions
+
+If you have any questions, please reach out:
+
+- [GitHub Profile](https://github.com/${answers.username})
+- Email: ${answers.email}
+`;
+
+
