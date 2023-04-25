68a9ad684840604038d11e8bd2054278f7c70e76
Evan <evanknight18@gmail.com>
2023-04-24 18:30:28 -0600

    added the user input prompt

--- index.js
+++ index.js
@@ -58,4 +58,58 @@ If you have any questions, please reach out:
 - Email: ${answers.email}
 `;
 
-
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter your project title:",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter installation instructions:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Enter usage information:",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Enter contribution guidelines:",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Enter test instructions:",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose a license for your application:",
+      choices: ["MIT", "GPLv3"],
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Enter your GitHub username:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address:",
+    },
+  ])
+  .then((answers) => {
+    const readmeContent = generateReadme(answers);
+    fs.writeFile("README.md", readmeContent, (err) =>
+      err ? console.log(err) : console.log("README.md generated successfully!")
+    );
+  });
